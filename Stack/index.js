f177fe64270fe8019103b35959c50041ad8ba8f2
Komenpurity <jepkorirpurity8@gmail.com>
2023-09-20 10:50:28 +0300

    added push and pop stack functions

--- Stack/index.js
+++ Stack/index.js
@@ -3,3 +3,28 @@
 // The last item you put in the stack is the first item that comes out of the stack
 // functions in a stack: push,pop,peek,length
 
+let letters = []; //this is our stack
+
+let word = "racecar"
+
+let rword = ""
+
+//put letters of word into stack
+for(let i=0; i<word.length; i++){
+    letters.push(word[i]);
+    console.log(letters) 
+}
+
+
+//remove letters on top of stack and adding it to rword variable
+for(let i=0; i<word.length; i++){
+    rword += letters.pop() 
+    console.log(letters)
+    console.log(rword) 
+}
+
+if(rword === word){
+    console.log(`${word} is a palindrome`) 
+} else {
+    console.log(`${word} is not a palindrome`)
+}
