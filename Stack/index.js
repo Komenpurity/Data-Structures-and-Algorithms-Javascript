8ed05f1b3d580634ebd684748071ce8a9228397d
Komenpurity <jepkorirpurity8@gmail.com>
2023-09-20 10:31:26 +0300

    added a stack folder

---
+++ Stack/index.js
@@ -0,0 +1,5 @@
+// A good example of a stack is a stack of books
+// You can only insert or delete items at the top of the stack
+// The last item you put in the stack is the first item that comes out of the stack
+// functions in a stack: push,pop,peek,length
+
